2cc067ba13c5903bc61e18d6ebe995d5b186ae57
mallesh120 <mallesh120@gmail.com>
2025-08-16 08:01:05 -0400

    Revert "Initial commit for path to divinity"
    This reverts commit 23ab4b5901c1e6023302af336815daff4fe75d82.

--- src/data/literature/index.js
+++
@@ -1,7 +0,0 @@
-import { ramayanaObject } from './ramayana/index.js';
-import { mahabharataObject } from './mahabharata.js';
-
-export const literatureData = [
-  ramayanaObject,
-  mahabharataObject,
-];
